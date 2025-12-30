af8f971fa1aa5e1f19629b319a2c07c95c09b666
msga-mmm <109926431+msga-mmm@users.noreply.github.com>
2025-12-30 15:28:18 -0500

    fix: avoid scan button being overlapped with app bar (#2)
    * fix: avoid scan button being overlapped with app bar
    
    * fix: use correct imports

--- composeApp/src/androidMain/kotlin/app/galletascanner/dev/App.kt
+++ composeApp/src/androidMain/kotlin/app/galletascanner/dev/App.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -200,6 +201,7 @@ actual fun App() {
                     Modifier
                         .fillMaxSize()
                         .verticalScroll(rememberScrollState())
+                        .systemBarsPadding()
                         .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(16.dp),
